a89389680eb8873dcd5a6d388a9a5cdb49d18be4
Thomas619233808 <59308022+Thomas619233808@users.noreply.github.com>
2023-11-20 17:20:06 +0800

    Update and rename 01_冒泡排序.py to S01_List.py

--- StudyDataStructure/S03排序/01_冒泡排序.py
+++
@@ -1,9 +0,0 @@
-def hanoi(n, a, b, c):
-    # 参数a，b，c是珠子的名称
-    # 函数hanoi()表示第n个盘子，从a开始移动，经过b，最终到达c
-    if n > 0:
-        hanoi(n - 1, a, c, b)
-        print(f"把{n}号圆盘从{a}位置移动到啊{c}位置")
-        hanoi(n - 1, b, a, c)
-
-hanoi(3, "A", "B", "C")
